b10a3cc7f41c6db356c5e311dcf95351d18f774c
SteveLee <SI.Lee@tsb.co.kr>
2022-08-05 11:33:34 +0900

    ttt

--- TestHello/src/TestHello.java
+++ TestHello/src/TestHello.java
@@ -21,7 +21,7 @@ public class TestHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!!");
-		System.out.println("Hello World2!!!");
+		System.out.println("Hello World2!!222!");
 	}
 
 }
